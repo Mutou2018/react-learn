56d29aaed5f018f9aef5a6a15df01f9174506f2c
郭杰 <guojie@synpowertech.com>
2020-02-25 17:28:52 +0800

    demo

---
+++ JianShu/src/pages/detail/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Detail extends Component {
+  render() {
+    return (
+      <div className="container">
+        Detail
+      </div>
+    )
+  }
+}
+
+export default Detail;
